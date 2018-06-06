b03713299cdfdac5cab57daaed178e8855e39dc7
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-06-05 18:21:51 -0700

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,117 +1,135 @@
-$(document).ready(function() {
+//Variables
+let compPick = '';
+let gemOne = '';
+let gemTwo = '';
+let gemThree = '';
+let gemFour = '';
+let wins;
+let losses;
+let score;
 
 
 
-	var compPick = Math.floor(Math.random() * 102) + 19; 
-		console.log("compPick: " + compPick); 
-		$(".randomNumber").html(compPick); 
+$(document).ready(function () {
 
-	// var randomArray = []; 
-	// 	console.log("randomArray: " + randomArray); 
 
-	// for (i = 0 ; i < 4 ; i++) {
-	// 	var gemNumber = Math.floor(Math.random() * 12) + 1; 
-	// 		console.log("gemNumber: " + gemNumber); 
-	// 	randomArray.push(gemNumber); 
-	// }
+	//Calculating random number for compPick between 19-120 and the random number generated from compPick will 
+	//appear on the Random Number section of the HTML page.
 
-	var gemOne = Math.floor(Math.random() * 12) + 1; 
-		console.log("Gem 1: " + gemOne); 
-		$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + ">"); 
+	let compPick = Math.floor(Math.random() * 120) + 19;
+	console.log("compPick: " + compPick);
+	$(".randomNumber").html(compPick);
 
-	var gemTwo = Math.floor(Math.random() * 12) + 1; 
-		console.log("Gem 2: " + gemTwo); 
-		$("#img2").html("<img src=" + "./assets/images/emerald.jpeg" + " value=" + gemTwo + ">"); 
+	//Calculating random number from 1-12 for all gems and displaying on HTML page. The random number displayed per gem is given by "value"
+	let gemOne = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 1: " + gemOne);
+	$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + "/>");
 
-	var gemThree = Math.floor(Math.random() * 12) + 1; 
-		console.log("Gem 3: " + gemThree); 
-		$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + ">");
-	
-	var gemFour = Math.floor(Math.random() * 12) + 1; 
-		console.log("Gem 4: " + gemFour); 
-		$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpeg" + " value=" + gemFour + ">");
-		
-	var wins = 0; 
-		console.log("wins: " + wins); 
+	let gemTwo = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 2: " + gemTwo);
+	$("#img2").html("<img src=" + "./assets/images/emerald.jpeg" + " value=" + gemTwo + "/>");
 
-	var losses = 0; 
-		console.log("losses: " + losses); 
+	let gemThree = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 3: " + gemThree);
+	$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + "/>");
 
-	var score = 0; 
-		console.log("score: " + score); 
+	let gemFour = Math.floor(Math.random() * 12) + 1;
+	console.log("Gem 4: " + gemFour);
+	$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpeg" + " value=" + gemFour + "/>");
 
-	function reset () {
-		compPick = Math.floor(Math.random() * 102) + 19; 
-			console.log("compPick: " + compPick); 
-		$(".randomNumber").html(compPick); 
 
-		score = 0; 
-		$(".scoreDisplay").html(score); 
 
-		gemOne = Math.floor(Math.random() * 12) + 1;  
-			console.log("Gem 1: " + gemOne); 
-		$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + ">"); 
+	//How to create a new score when the gem image is clicked
+	$("img").on("click", function () {
+		let newScore = score += parseInt($(this).attr("value"));
+		console.log("New Score: " + newScore);
+		$(".scoreDisplay").html(newScore);
 
-		gemTwo = Math.floor(Math.random() * 12) + 1; 
-			console.log("Gem 2: " + gemTwo); 
-		$("#img2").html("<img src=" + "./assets/images/emerald.jpg" + " value=" + gemTwo + ">"); 
+		if (newScore === compPick) {
+			wins++;
+			$(".wins").html("Wins: " + wins);
+			console.log("Wins: " + wins);
+			reset();
+		}
 
-		gemThree = Math.floor(Math.random() * 12) + 1; 
-			console.log("Gem 3: " + gemThree); 
-		$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + ">");
-	
-		gemFour = Math.floor(Math.random() * 12) + 1; 
-			console.log("Gem 4: " + gemFour); 
-		$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpg" + " value=" + gemFour + ">");
+		else if (newScore > compPick) {
+			losses++;
+			$(".losses").html("Losses: " + losses);
+			console.log("Losses: " + losses);
+			reset();
+		}
+
+	});
+
+
+
+
+
+
+	//How to Reset Game
+	let wins = 0;
+	console.log("wins: " + wins);
+
+	let losses = 0;
+	console.log("losses: " + losses);
+
+	let score = 0;
+	console.log("score: " + score);
+
+
+	function reset() {
+		compPick = Math.floor(Math.random() * 120) + 19;
+		console.log("compPick: " + compPick);
+		$(".randomNumber").html(compPick);
+
+		score = 0;
+		$(".scoreDisplay").html(score);
+
+		//gemOne = Math.floor(Math.random() * 12) + 1;  
+		console.log("Gem 1: " + gemOne);
+		//$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + "/>"); 
+
+		//gemTwo = Math.floor(Math.random() * 12) + 1; 
+		console.log("Gem 2: " + gemTwo);
+		//$("#img2").html("<img src=" + "./assets/images/emerald.jpg" + " value=" + gemTwo + "/>"); 
+
+		//gemThree = Math.floor(Math.random() * 12) + 1; 
+		console.log("Gem 3: " + gemThree);
+		//$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + "/>");
+
+		//gemFour = Math.floor(Math.random() * 12) + 1; 
+		console.log("Gem 4: " + gemFour);
+		//$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpg" + " value=" + gemFour + "/>");
+
 
 		$("img").on("click", function () {
-			var newScore = score += parseInt($(this).attr("value")); 
-				console.log("New Score: " + newScore); 
-			$(".scoreDisplay").html(newScore); 
+			let newScore = score += parseInt($(this).attr("value"));
+			console.log("New Score: " + newScore);
+			$(".scoreDisplay").html(newScore);
 
-			if(newScore === compPick) { 
-				wins++ ; 
-				$(".wins").html("Wins: " + wins); 
-					console.log("Wins: " + wins); 
-					reset(); 
-			
-			} 
+			if (newScore === compPick) {
+				wins++;
+				$(".wins").html("Wins: " + wins);
+				console.log("Wins: " + wins);
+				reset();
 
-			else if(newScore > compPick) {
-				losses++ ; 
-				$(".losses").html("Losses: " + losses); 
-					console.log("Losses: " + losses); 
-					reset(); 
-				
 			}
 
-		}); 
+			else if (newScore > compPick) {
+				losses++;
+				$(".losses").html("Losses: " + losses);
+				console.log("Losses: " + losses);
+				reset();
+
+			}
+
+		});
 
 
 
 	}
+	//Finished with reset game code above.
 
-	$("img").on("click", function () {
-		var newScore = score += parseInt($(this).attr("value")); 
-			console.log("New Score: " + newScore); 
-		$(".scoreDisplay").html(newScore); 
 
-		if(newScore === compPick) { 
-			wins++ ; 
-			$(".wins").html("Wins: " + wins); 
-				console.log("Wins: " + wins); 
-				reset(); 
-		} 
-
-		else if(newScore > compPick) {
-			losses++ ; 
-			$(".losses").html("Losses: " + losses); 
-				console.log("Losses: " + losses); 
-				reset(); 
-		}
-
-	}); 
 
 }); 
-
-

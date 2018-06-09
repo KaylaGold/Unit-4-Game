dd4aef4b4f2fa9acf568267de9f166d32a567af2
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-06-08 22:46:06 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,36 +4,36 @@ let gemOne = '';
 let gemTwo = '';
 let gemThree = '';
 let gemFour = '';
-let wins;
-let losses;
-let score;
+let wins = 0;
+let losses = 0;
+let score = 0;
 
 
 
 $(document).ready(function () {
 
 
-	//Calculating random number for compPick between 19-120 and the random number generated from compPick will 
-	//appear on the Random Number section of the HTML page.
+	/*Calculating random number for compPick between 19-120 and the random number generated from compPick will 
+	appear on the Random Number section of the HTML page.*/
 
-	let compPick = Math.floor(Math.random() * 120) + 19;
+	let compPick = Math.floor(Math.random() * 101) + 19;
 	console.log("compPick: " + compPick);
 	$(".randomNumber").html(compPick);
 
 	//Calculating random number from 1-12 for all gems and displaying on HTML page. The random number displayed per gem is given by "value"
-	let gemOne = Math.floor(Math.random() * 12) + 1;
+	let gemOne = Math.floor(Math.random() * 11) + 1;
 	console.log("Gem 1: " + gemOne);
 	$("#img1").html("<img src=" + "./assets/images/redcrystal2.png" + " value=" + gemOne + ">");
 
-	let gemTwo = Math.floor(Math.random() * 12) + 1;
+	let gemTwo = Math.floor(Math.random() * 11) + 1;
 	console.log("Gem 2: " + gemTwo);
 	$("#img2").html("<img src=" + "./assets/images/emerald2.png" + " value=" + gemTwo + ">");
 
-	let gemThree = Math.floor(Math.random() * 12) + 1;
+	let gemThree = Math.floor(Math.random() * 11) + 1;
 	console.log("Gem 3: " + gemThree);
 	$("#img3").html("<img src=" + "./assets/images/bluecrystal2.png" + " value=" + gemThree + ">");
 
-	let gemFour = Math.floor(Math.random() * 12) + 1;
+	let gemFour = Math.floor(Math.random() * 11) + 1;
 	console.log("Gem 4: " + gemFour);
 	$("#img4").html("<img src=" + "./assets/images/yellowcrystal2.png" + " value=" + gemFour + ">");
 
@@ -78,28 +78,28 @@ $(document).ready(function () {
 
 
 	function reset() {
-		compPick = Math.floor(Math.random() * 120) + 19;
+		compPick = Math.floor(Math.random() * 101) + 19;
 		console.log("compPick: " + compPick);
 		$(".randomNumber").html(compPick);
 
 		score = 0;
 		$(".scoreDisplay").html(score);
 
-		//gemOne = Math.floor(Math.random() * 12) + 1;  
+		gemOne = Math.floor(Math.random() * 11) + 1;  
 		console.log("Gem 1: " + gemOne);
-		//$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + "/>"); 
+		$("#img1").html("<img src=" + "./assets/images/redcrystal.jpg" + " value=" + gemOne + ">"); 
 
-		//gemTwo = Math.floor(Math.random() * 12) + 1; 
+		gemTwo = Math.floor(Math.random() * 11) + 1; 
 		console.log("Gem 2: " + gemTwo);
-		//$("#img2").html("<img src=" + "./assets/images/emerald.jpg" + " value=" + gemTwo + "/>"); 
+		$("#img2").html("<img src=" + "./assets/images/emerald.jpg" + " value=" + gemTwo + ">"); 
 
-		//gemThree = Math.floor(Math.random() * 12) + 1; 
+		gemThree = Math.floor(Math.random() * 11) + 1; 
 		console.log("Gem 3: " + gemThree);
-		//$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + "/>");
+		$("#img3").html("<img src=" + "./assets/images/bluecrystal.jpg" + " value=" + gemThree + ">");
 
-		//gemFour = Math.floor(Math.random() * 12) + 1; 
+		gemFour = Math.floor(Math.random() * 11) + 1; 
 		console.log("Gem 4: " + gemFour);
-		//$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpg" + " value=" + gemFour + "/>");
+		$("#img4").html("<img src=" + "./assets/images/yellowcrystal.jpg" + " value=" + gemFour + ">");
 
 
 		$("img").on("click", function () {
@@ -120,11 +120,10 @@ $(document).ready(function () {
 				$(".losses").html("Losses: " + losses);
 				console.log("Losses: " + losses);
 				reset();
-
+				
 			}
 
 		});
-
 
 
 	}
